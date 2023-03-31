36bef16fca26147ae45b2c24fc4603117114417a
jayoung choi <61008837+jayoung977@users.noreply.github.com>
2023-04-01 00:56:25 +0900

    [Bronze II] Title: 바구니 순서 바꾸기, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10812. 바구니 순서 바꾸기/바구니 순서 바꾸기.py
+++ 백준/Bronze/10812. 바구니 순서 바꾸기/바구니 순서 바꾸기.py
@@ -2,6 +2,6 @@ N, M = map(int,input().split())
 basket = [ i+1 for i in range(N)]
 for n in range(M):
     i,j,k = map(int,input().split())
-    basket = basket[:i-1] +basket[k-1:j] + basket[i-1:k-1] + basket[j:]
+    basket = basket[:i-1] + basket[k-1:j] + basket[i-1:k-1] + basket[j:]
 
 print(' '.join(map(str,basket)))
