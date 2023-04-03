d202e1517e7302eb97490b5ac5f6158fad0b84cc
jayoung choi <61008837+jayoung977@users.noreply.github.com>
2023-04-03 22:45:49 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
@@ -0,0 +1,13 @@
+num = int(input())
+per_li = []
+for i in range(num):
+    li = list(map(int,input().split()))
+    total_num = li[0]
+    score_li = li[1:]
+    mean = sum(score_li) / total_num
+    upper_std = len([ i for i in score_li if i > mean])
+    per =  "%0.3f%%" % (upper_std/total_num*100)
+    per_li.append(per)
+    
+for p in per_li:
+    print(p)
