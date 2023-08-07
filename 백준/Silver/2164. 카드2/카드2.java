d912d49a406a5f12eb5a6963752dcf46207b185b
jayoung choi <61008837+jayoung977@users.noreply.github.com>
2023-08-07 13:33:37 +0900

    [Silver IV] Title: 카드2, Time: 184 ms, Memory: 28940 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -4,21 +4,21 @@ import java.util.*;
 
 public class Main {
 	static int N;
-	static Queue<Integer> q ;
+	static Deque<Integer> deque ;
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		q = new LinkedList<>();
+		deque = new ArrayDeque<>();
 		
 		for(int i = 1; i <= N; i++) {
-			q.offer(i);
+			deque.addLast(i);
 		}
-		while(q.size() > 1) {
-			q.poll();	// 맨 앞의 원소 버림 
-			q.offer(q.poll());	// 맨 앞의 원소를 버림과 동시에 버려진 원소를 맨 뒤에 삽입 
+		while(deque.size() > 1) {
+			deque.pollFirst();	// 맨 앞의 원소 버림 
+			deque.addLast(deque.pollFirst());	// 맨 앞의 원소를 버림과 동시에 버려진 원소를 맨 뒤에 삽입 
 		}
 		
-		System.out.println(q.poll());	// 마지막으로 남은 원소 출력 
+		System.out.println(deque.pollFirst());	// 마지막으로 남은 원소 출력 
 
 	}
 
