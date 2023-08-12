1b53dd297e8bd0809e3f630747448dd6a5d2e9ff
jayoung choi <61008837+jayoung977@users.noreply.github.com>
2023-08-12 15:26:15 +0900

    [Silver IV] Title: 색종이 - 2, Time: 80 ms, Memory: 11640 KB -BaekjoonHub

--- 백준/Silver/2567. 색종이 － 2/색종이 － 2.java
+++ 백준/Silver/2567. 색종이 － 2/색종이 － 2.java
@@ -1,42 +1,33 @@
-
-
-import java.io.*;
 import java.util.*;
-
+import java.io.*;
 public class Main {
-	static int[][] map;
 	static int N;
-	static int answer;
+	static int [][] map;
+	static int cnt; 
 	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		N = Integer.parseInt(br.readLine());
-		map = new int[101][101];
-		answer = 0;
+		map = new int[101][101]; //도화지, 자연수 입력은 그냥 101
+        cnt = 0;
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
-			int r =  Integer.parseInt(st.nextToken());
-			int c =  Integer.parseInt(st.nextToken());
+			int r = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
 			for (int j = r; j < r+10; j++) {
 				for (int k = c; k < c+10; k++) {
-					map[j][k] = 1;
+					map[j][k]=1;
 				}
 			}
-			
 		}
+		//범위가 100까지라 k+1쓰면 안되고 k-1써야
 		for (int j = 1; j < 101; j++) {
 			for (int k = 1; k < 101; k++) {
-				if( map[j][k] != map[j][k-1] ) {
-					answer++;
-				}	
-				if(  map[k][j] != map[k-1][j]) {
-					answer++;
-				}
+				if(map[j][k-1]!=map[j][k]) cnt++;
+				if(map[k-1][j]!=map[k][j]) cnt++;
 			}
-				
 		}
-		System.out.println(answer);
-
+		System.out.println(cnt);
 	}
 
 }
