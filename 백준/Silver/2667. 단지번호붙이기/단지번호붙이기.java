04066d24270c36b8c8459d1f29d8fbfd3a72b2a3
jayoung choi <61008837+jayoung977@users.noreply.github.com>
2023-08-13 17:48:35 +0900

    [Silver I] Title: 단지번호붙이기, Time: 96 ms, Memory: 11632 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -7,64 +7,58 @@ public class Main {
 	static boolean[][] visited;
 	static int[] dr = {1,0,-1,0};
 	static int[] dc = {0,1,0,-1};
-	static int cnt;
+	static int group_n;
+	static int[] groups = new int[25*25];
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
 		map = new int[N][N];
-		cnt = 0;
+		visited = new boolean[N][N];
+		group_n = 0;
 		for (int i = 0; i < N; i++) {
 			String str = br.readLine();
 			char[] cs = str.toCharArray();
 			for (int j = 0; j < N; j++) {
 				map[i][j] = cs[j]-'0';
 			}
-			
 		}
-
-		for  (int i = 0; i < N; i++) {
+		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
-				if(map[i][j]==1) {
-					cnt++;
-					dfs(i,j,cnt+1);
-				}
-
-				
-			}
-		}
-		System.out.println(cnt);
-		int[] res = new int[cnt];
-		for (int k = 2; k < cnt+2; k++) {
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < N; j++) {
-					if(map[i][j]==k) {
-						res[k-2]++;
-					}
+				if(map[i][j]==1 && !visited[i][j]) {
+					group_n++;
+					dfs(i,j);
 				}
 			}
-			
 		}
-		Arrays.sort(res);
-		for(int r:res) {
-			System.out.println(r);
+		System.out.println(group_n);
+		Arrays.sort(groups);
+		for(int g:groups) {
+			if(g==0) continue;
+			System.out.println(g);
 		}
 		
 
-		
 	}
-	static void dfs(int r, int c, int g) {
-		map[r][c] = g;
-		for (int j = 0; j < 4; j++) {
-			int nr = r+dr[j];
-			int nc = c+dc[j];
+
+
+
+	static void dfs(int r, int c) {
+		visited[r][c]= true;
+		groups[group_n]++;
+		for (int d = 0; d < 4; d++) {
+			int nr = r + dr[d];
+			int nc = c + dc[d];
 			if(!check(nr,nc)) continue;
-			if(map[nr][nc]==1) {
-				map[nr][nc]=g;
-				dfs(nr,nc,g);
-			}	
+			if(map[nr][nc]==1 && !visited[nr][nc]) {
+				dfs(nr,nc);
+			}
 		}
 		
+		
 	}
+
+
+
 	static boolean check(int r, int c) {
 		return r >= 0 && c >=0 && r <N && c<N;
 	}
