0f3c326bdc99bf8b9b22a2ff9e18ce660e102754
jayoung choi <61008837+jayoung977@users.noreply.github.com>
2023-08-20 17:12:53 +0900

    [Silver I] Title: 오목, Time: 84 ms, Memory: 11696 KB -BaekjoonHub

--- 백준/Silver/2615. 오목/오목.java
+++ 백준/Silver/2615. 오목/오목.java
@@ -1,59 +1,147 @@
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
-public class Main{
-
-	static int N=19;
-	static int[][] map;
-	// 정방향
-	//static int [] rdr= {-1,0,1,1};
-	//static int [] rdc= { 1,1,1,0};
-	// 역방향
-	//static int [] dr= { 1, 0,-1,-1};
-	//static int [] dc= {-1,-1,-1, 0};
-	                   // 역방향           // 정방향
-	static int [] dr= { 0,-1, 1,-1, 1,-1,1,0};
-	static int [] dc= {-1,-1,-1, 0, 0, 1,1,1};
+import java.io.*;
+import java.util.*;
+public class Main {
+	static final int N = 19;
+	static int [][] map = new int[N+2][N+2];
+	static int[] dr = {-1,0,-1,1};
+	static int[] dc = {-1,-1,0,-1};
 	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st=null;
-		map=new int[N+2][N+2];  //1흑 2백 0빔
+    static int[] dr1 = {1,2,3,4,5};
+    static int[] dc1 = {1,2,3,4,5};
+    static int[] dr2 = {0,0,0,0,0};
+    static int[] dc2 = {1,2,3,4,5};
+    static int[] dr3 = {1,2,3,4,5}; 
+    static int[] dc3 = {0,0,0,0,0};
+    static int[] dr4 = {-1,-2,-3,-4,-5}; 
+    static int[] dc4 = {1,2,3,4,5};
+    static int whoWin = 0;
+    static int winr = 0;
+    static int winc = 0;
+    static Queue<int[]> que;
+    static int cnt1,cnt2,cnt3,cnt4;
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		que = new LinkedList<int[]>();
 		for (int i = 1; i < N+1; i++) {
-			st=new StringTokenizer(br.readLine());
+			StringTokenizer st = new StringTokenizer(br.readLine());
 			for (int j = 1; j < N+1; j++) {
-				map[i][j]=Integer.parseInt(st.nextToken());
-			}
-		}
-		// 로직
-		for (int r = 1; r < N+1; r++) {
-			for (int c = 1; c < N+1; c++) {  // 모든 좌표를 확인해보자. 
-				if(map[r][c]!=0) {           // 흑이나 백에서 시작
-					for (int d = 0; d <4; d++) { // 4 방향으로 같은 색이 있는가 체크
-						int nr=r+dr[d];
-						int nc=c+dc[d];
-						if(map[nr][nc]==map[r][c]) continue; // 역방항 한개 같은색
-						// 정방향으로 연속으로 오직 5개 -> 원하는 결과!!
-						if(steps(map[r][c],r,c,7-d)) {
-							System.out.println(map[r][c]);
-							System.out.println(r+" "+c);
-							return ;
-						}
-					}
+				map[i][j] = Integer.parseInt(st.nextToken());
+				if(map[i][j]!=0) {
+					que.offer(new int[] {i,j});
 				}
 			}
 		}
-		System.out.println("0");
+//		for (int i = 0; i < N; i++) {
+//			System.out.println(Arrays.toString(map[i]));
+//		}
+		while(!que.isEmpty()) {
+			int[] cur = que.poll();
+			if(winCheck(cur[0], cur[1])) {
+				whoWin= map[cur[0]][cur[1]];
+				winr = cur[0];
+				winc = cur[1];
+			}
+		}
+		if(whoWin==0) {
+			System.out.println(0);
+			return;
+		}
+		System.out.println(whoWin);
+		System.out.println(winr+" "+winc);
+		
+		
 	}
 
-	static boolean steps(int v, int r, int c, int d) {
-		int cnt=1;
-		for (; v ==map[r+dr[d]][c+dc[d]]; r+=dr[d],c+=dc[d]) {
-			cnt++;
+	private static boolean winCheck(int r, int c) {
+		int who = map[r][c];
+		cnt1 = 1;
+		cnt2 = 1;
+		cnt3 = 1;
+		cnt4 = 1;
+		for (int d = 0; d < 4; d++) {
+			int nr=r+dr[d]; 
+			int nc=c+dc[d]; 
+			if(!check(nr,nc)) continue;
+			if(map[nr][nc]==who)continue;
+			if(d==0) { if(check1(r,c,who)) return true;}
+			else if(d==1) { if(check2(r,c,who)) return true;}
+			else if (d==2){if(check3(r,c,who)) return true;}
+			else {if(check4(r,c,who)) return true;}
 		}
-		return cnt==5?true:false;
+		return false;
+
+		
 	}
+
+
+
+
+
+	private static boolean check1(int r, int c,int who) {
+		for (int d = 0; d < 5; d++) {
+			int nr=r+dr1[d]; 
+			int nc=c+dc1[d]; 
+			if(!check(nr,nc)) break;
+			if(map[nr][nc]!=who) break;
+			else if(map[nr][nc]==who) {
+				cnt1++;
+			}	
+		}
+		if(cnt1==5) {
+			return true;
+		}
+		return false;
+	}
+
+	private static boolean check2(int r, int c, int who) {
+		for (int d = 0; d < 5; d++) {
+			int nr=r+dr2[d]; 
+			int nc=c+dc2[d]; 
+			if(!check(nr,nc)) break;
+			if(map[nr][nc]!=who) break;
+			else if(map[nr][nc]==who) {
+				cnt2++;
+			}	
+		}
+		if(cnt2==5) {
+			return true;
+		}
+		return false;
+	}
+	
+	private static boolean check3(int r, int c, int who) {
+		for (int d = 0; d < 5; d++) {
+			int nr=r+dr3[d]; 
+			int nc=c+dc3[d]; 
+			if(!check(nr,nc)) break;
+			if(map[nr][nc]!=who) break;
+			else if(map[nr][nc]==who) {
+				cnt3++;
+			}	
+		}
+		if(cnt3==5) {
+			return true;
+		}
+		return false;
+	}
+	private static boolean check4(int r, int c, int who) {
+		for (int d = 0; d < 5; d++) {
+			int nr=r+dr4[d]; 
+			int nc=c+dc4[d]; 
+			if(!check(nr,nc)) break;
+			if(map[nr][nc]!=who) break;
+			else if(map[nr][nc]==who) {
+				cnt4++;
+			}	
+		}
+		if(cnt4==5) {
+			return true;
+		}
+		return false;
+	}
+	
+	private static boolean check(int r, int c) {
+		return r>=0 && c>=0 && r<N+2 && c<N+2 ;
+	}
+
 }
