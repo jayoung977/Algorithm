649e59878057dbde35087b207f6930afa1438688
jayoung choi <61008837+jayoung977@users.noreply.github.com>
2023-08-29 14:05:45 +0900

    [Silver III] Title: 2×n 타일링, Time: 112 ms, Memory: 12892 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -1,27 +1,14 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-
-public class Main {
-
-	static int N;
-	static int[] D;
-
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		N = Integer.parseInt(br.readLine());
-		D = new int[1001];
-		Arrays.fill(D, -1); // 메모되지 않은 상태를 나타내는 값으로 초기화
-		D[1] = 1;
-		D[2] = 2;
-
+import java.util.Scanner;
+public class Main{
+	public static void main(String[] args) {
+		Scanner scann = new Scanner(System.in);
+		int N = scann.nextInt();
+		int[] dp = new int[1001];
+		dp[1]=1;
+		dp[2]=2;
 		for (int i = 3; i <= N; i++) {
-			D[i] = D[i - 1] + D[i - 2];
-			D[i] %= 10007;
+			dp[i]= (dp[i-1]+dp[i-2])%10007;			
 		}
-
-		System.out.println(D[N]);
+		System.out.println(dp[N]);
 	}
 }
