8489230f06fb52785956b203fb1e297c1e3f1fd4
jayoung choi <61008837+jayoung977@users.noreply.github.com>
2023-08-30 17:32:29 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 584 ms, Memory: 61588 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
@@ -1,4 +1,3 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,13 +7,10 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-	static int[] dr = { 0, 1, 0, -1, -1, -2, -2, -1, 1, 2, 2, 1 };
-	static int[] dc = { 1, 0, -1, 0, -2, -1, 1, 2, 2, 1, -1, -2 };
-	// static int[] dr = { 0, 1, 0, -1 };
-	// static int[] dc = { 1, 0, -1, 0 };
+	static int[] dr = { 0, 1, 0, -1, -1, -2, -2, -1, 1, 2, 2, 1 }; //행 이동. 인덱스 0~3 : 한 칸 이동, 인덱스 4~11 : 말처럼 이동
+	static int[] dc = { 1, 0, -1, 0, -2, -1, 1, 2, 2, 1, -1, -2 }; //열 이동. 인덱스 0~3 : 한 칸 이동, 인덱스 4~11 : 말처럼 이동
 	static int K, W, H;
 	static int[][] map;
-	static int[][][] dist;
 	static boolean[][][] visited;
 	static int cnt;
 
@@ -23,13 +19,13 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 
-		K = Integer.parseInt(br.readLine());
+		K = Integer.parseInt(br.readLine()); //K번 말처럼 이동할 수 있음
+		
 		st = new StringTokenizer(br.readLine());
 		W = Integer.parseInt(st.nextToken()); // 가로 길이. 열의 수
 		H = Integer.parseInt(st.nextToken()); // 세로 길이. 행의 수
-		map = new int[H][W];
-		dist = new int[H][W][K + 1];
-		visited = new boolean[H][W][K + 1];
+		map = new int[H][W]; //격자판
+		visited = new boolean[H][W][K + 1]; //방문 체크
 
 		for (int i = 0; i < H; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -37,16 +33,16 @@ public class Main {
 				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-
 		// ==================입력 완료
 
 		System.out.println(bfs());
 
 	}
 
+	//시작지점에서 도착지점까지의 거리를 리턴하는 함수
 	static int bfs() {
 		Queue<int[]> q = new ArrayDeque<>();
-		q.offer(new int[] { 0, 0, 0 }); // 0,0에서 시작
+		q.offer(new int[] { 0, 0, 0 }); // 0,0에서 시작(말처럼 0번 이동 상태)
 		visited[0][0][0] = true;
 
 		while (!q.isEmpty()) {
@@ -57,54 +53,48 @@ public class Main {
 				int c = cur[1];
 				int k = cur[2];
 
-				if (r == H - 1 && c == W - 1) {
-					return cnt;
+				if (r == H - 1 && c == W - 1) { //도착지점에 도착하면
+					return cnt; //너비 리턴
 				}
 
-				int size = 0; // 방향 배열 사용 크기
-				// 말처럼 K번 이동했으면 상하좌우로만 이동 가능
+				int size = 0; // 방향 배열 사용 크기 (인덱스0~3만 쓸 것인지, 12개 모두 쓸 것인지)
+				// 말처럼 K번 이동 안 했으면 12개 방향 모두 가능
 				if (k < K) {
 					size = 12;
 				}
-				// 말처럼 K번 이동 안 했으면 12개 방향 모두 가능
+				// 말처럼 K번 이동했으면 상하좌우로만 이동 가능
 				else {
 					size = 4;
 				}
-				for (int d = 0; d < size; d++) {
+				for (int d = 0; d < size; d++) { //저장한 사용 크기만큼만 for문을 돌림
 					int nr = r + dr[d];
 					int nc = c + dc[d];
 
-					if (!check(nr, nc)) {
+					if (!check(nr, nc)) { //범위를 벗어난 경우
 						continue;
 					}
-
-					if (map[nr][nc] == 1) {
+					if (map[nr][nc] == 1) { //벽을 만난 경우
 						continue;
 					}
-
-					if (d >= 4) {
+					if (d >= 4) { //말처럼 이동하는 경우
 						if (visited[nr][nc][k + 1]) {
 							continue;
 						}
-						q.offer(new int[] { nr, nc, k + 1 });
-						visited[nr][nc][k + 1] = true;
+						q.offer(new int[] { nr, nc, k + 1 }); //말처럼 이동하므로 k+1
+						visited[nr][nc][k + 1] = true; //방문 처리
 					}
-
-					if (d < 4) {
+					if (d < 4) { //말처럼 이동하지 않고 그냥 이동하는 경우
 						if (visited[nr][nc][k]) {
 							continue;
 						}
-						q.offer(new int[] { nr, nc, k });
-						visited[nr][nc][k] = true;
+						q.offer(new int[] { nr, nc, k }); //그냥 이동하므로 k 유지
+						visited[nr][nc][k] = true; //방문 처리
 					}
-
 				}
-
 			}
-			cnt++;
+			cnt++; //qsize만큼 돌고 나면 너비가 증가함
 		}
-		return -1;
-
+		return -1; //q가 빌 때까지 도착지점에 다다르지 못한 경우 -1 리턴
 	}
 
 	static boolean check(int r, int c) {
