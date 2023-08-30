9ede429881482fede3163b91fcebc66e021a5965
jayoung choi <61008837+jayoung977@users.noreply.github.com>
2023-08-30 13:49:57 +0900

    [Silver I] Title: RGB거리, Time: 96 ms, Memory: 12104 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,43 +1,47 @@
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
+/*
+ 조건:
+ 1번 집부터 N번 집이 순서대로 있다.  N(2 ≤ N ≤ 1,000)
+ 집은 빨강, 초록, 파랑 중 하나의 색으로 칠해야 한다
+ i(2 ≤ i ≤ N-1)번 집의 색은 i-1번, i+1번 집의 색과 같지 않아야 한다.
+ 집을 칠하는 비용은 1,000보다 작거나 같은 자연수이다.
+ 모든 집을 칠하는 비용의 최솟값
+ */
 public class Main {
 
-	static int N;
-	static int[][] cost;
-
-	public static void main(String[] args) throws NumberFormatException, IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		N = Integer.parseInt(br.readLine());
-		cost = new int[N + 1][3];
+		int N = Integer.parseInt(br.readLine());
 		StringTokenizer st;
-
-		for (int i = 1; i <= N; i++) {
+		int[][] D = new int[N+1][3];
+		int [][] map = new int[N+1][3];
+		for (int i = 1; i < N+1; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < 3; j++) {
-				cost[i][j] = Integer.parseInt(st.nextToken());
+				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-
-		int[][] D = new int[N + 1][3]; // 금액 n에 대한 최소 동전 수
-		// 점화식으로 채워질 수 없는 동적테이블의 값 초기화!
-		D[0][0] = 0;
-		D[0][1] = 0;
-		D[0][2] = 0;
-
-		for (int i = 1; i <= N; i++) {
-			D[i][0] = Math.min(D[i - 1][1] + cost[i][1], D[i - 1][2] + cost[i][2]);
-			D[i][1] = Math.min(D[i - 1][0] + cost[i][0], D[i - 1][2] + cost[i][2]);
-			D[i][2] = Math.min(D[i - 1][0] + cost[i][0], D[i - 1][1] + cost[i][1]);
+		for (int i = 0; i < 3; i++) {
+			D[1][i] = map[1][i];
+			
 		}
+		//입력 끝
+		for (int i = 2; i <=N; i++) {
+			//R 시도
+			D[i][0] = Math.min(D[i-1][1], D[i-1][2])+map[i][0];
+			//G 시도
+			D[i][1] = Math.min(D[i-1][0], D[i-1][2])+map[i][1];
+			//B 시도
+			D[i][2] = Math.min(D[i-1][0], D[i-1][1])+map[i][2];
 
-		int answer = Math.min(Math.min(D[N][0], D[N][1]), D[N][2]);
 
-		System.out.println(answer);
+			
+		}
+		System.out.println(Math.min(Math.min(D[N][0],D[N][1]),D[N][2]));
 
 	}
+
 }
