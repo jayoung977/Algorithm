175c73168b8e56898b9e7d89bfe70a8b80999c3b
jayoung choi <61008837+jayoung977@users.noreply.github.com>
2023-09-11 22:49:21 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 275.80 ms, Memory: 96 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,24 +2,16 @@ import java.io.*;
 import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        String answer = "";
-        Map <String,Integer> hm = new HashMap<>();
-        
-        for(String p:participant){
-            hm.put(p, hm.getOrDefault(p,0)+1);
-        }
-        for(String c:completion){
-            hm.put(c, hm.get(c)-1);
-        }
-        for(String key: hm.keySet()){
-            if(hm.get(key)!=0){
-                answer = key;
-                break;
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        for(int i=0; i< completion.length; i++){
+            if(participant[i].equals(completion[i])){
+                continue;
+            } else{
+                return participant[i];
             }
         }
-        
-        
-        return answer;
+        return participant[participant.length-1];
        
     }
 }
