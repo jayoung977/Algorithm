c1e23b8b21216c033caf5d3f31b5b17718065640
jayoung choi <61008837+jayoung977@users.noreply.github.com>
2023-09-12 20:35:24 +0900

    [level 1] Title: 폰켓몬, Time: 1.71 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -1,18 +1,12 @@
-import java.io.*;
 import java.util.*;
 class Solution {
     public int solution(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int n:nums){
+            set.add(n);
+        }
         int max = nums.length/2;
-        //중복 제거하기
-        HashSet<Integer> numSet = new HashSet<>();
-        for(int num:nums){
-            numSet.add(num);
-        }
-        //중복을 제거한 셋의 크기가 max보다 크면 max를 , 작으면 numSet의 size를 리턴
-        if(numSet.size() > max){
-            return max;
-        }else{
-            return numSet.size();
-        }
+        int answer = set.size() > max? max:set.size();
+        return answer;
     }
 }
