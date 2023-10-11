b4ac6ac6b4c5408274e0855bee9135c752660378
jayoung choi <61008837+jayoung977@users.noreply.github.com>
2023-10-11 21:53:12 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 172 ms, Memory: 17220 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -1,64 +1,75 @@
-import java.util.*;
 import java.io.*;
-public class Main {	
-	static int N;
-	static int[][] map;
-//	static int[][] visited;
-	static int answer;
-	static int testcase = 0;
+import java.util.*;
+public class Main {
+    static int N;
+    static class Node implements Comparable<Node>{
+    	int r,c,w;
+    	public Node(int r, int c, int w) {
+    		this.r=r;
+    		this.c=c;
+    		this.w=w;
+    	}
+    	@Override
+    	public int compareTo(Node o) {
+    		return Integer.compare(this.w, o.w);
+    	}
+    }
+    static int[][] visited;
+    static PriorityQueue<Node> points;
+    static int MM = Integer.MAX_VALUE/1000;
+    static int[][] map;
+    static int[] dr = {0,1,0,-1};
+    static int[] dc = {1,0,-1,0};
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
+		int t=1;
 		while(true) {
-			N=Integer.parseInt(br.readLine());
-			if(N==0) break;
-			testcase++;
+			N = Integer.parseInt(br.readLine());
+			if(N==0)break;
 			map=new int[N][N];
-			int[][] visited=new int[N][N];
-			answer=Integer.MAX_VALUE/1000;
 			for(int i=0; i<N; i++) {
-				st = new StringTokenizer(br.readLine());
+				st=new StringTokenizer(br.readLine());
 				for(int j=0; j<N; j++) {
 					map[i][j]=Integer.parseInt(st.nextToken());
-					visited[i][j]=Integer.MAX_VALUE;
 				}
 			}
-			visited[0][0] = map[0][0];
-			dfs(0,0,visited,map[0][0]);
-			System.out.println("Problem " + testcase + ": " +answer);
+			visited=new int[N][N];
+			for(int i=0; i<N; i++) Arrays.fill(visited[i], MM);
+			visited[0][0]=map[0][0];
+			
+			points=new PriorityQueue<>();
+			points.offer(new Node(0,0,map[0][0]));
+			
+			bfs();
+//			for(int i=0; i<N; i++)System.out.println(Arrays.toString(visited[i]));
+//			System.out.println("============================");
+			System.out.println("Problem "+t+": "+ visited[N-1][N-1]);
+			t++;
+			
+			
 			
 		}
-
 	}
-	static int[] dr={0,1,0,-1};
-	static int[] dc={1,0,-1,0};
-	static void dfs(int r, int c, int[][] visited, int tot) {
-		if(tot>=answer) return;
-		
-		if(r==N-1&&c==N-1) {
-//			System.out.println("tot:"+tot);
-			answer = Math.min(tot, answer);
-			return;
+	static void bfs() {
+		while(!points.isEmpty()) {
+			Node cur = points.poll();
+			if(cur.r==N-1 && cur.c==N-1) break;
+			for(int d=0; d<4; d++) {
+				int nr = cur.r+dr[d];
+				int nc = cur.c+dc[d];
+				if(!(nr>=0 && nc>=0 && nr<N && nc<N)) continue;
+				if(visited[nr][nc]!=MM) continue;
+				if(visited[nr][nc]>visited[cur.r][cur.c]+map[nr][nc]) {
+					visited[nr][nc]=visited[cur.r][cur.c]+map[nr][nc];
+//					System.out.println("visited[nr][nc]:"+visited[nr][nc]);
+					points.offer(new Node(nr,nc,visited[nr][nc]));
+				}
+			}
 		}
-		for(int d=0; d<4; d++) {
-			int nr=r+dr[d];
-			int nc=c+dc[d];
-			if(!(nr>=0 && nc>=0 && nr<N && nc<N)) continue;
-			if(visited[nr][nc]<=tot+map[nr][nc]) continue;
-	        
-			visited[nr][nc] = tot+map[nr][nc];
-			dfs(nr,nc,visited,tot+map[nr][nc]);
-	
-		}
-		
 	}
 
 }
-
 /*
-3
-5 5 4
-3 9 1
-3 2 7
-0 
+ 굳이 행렬을 인접리스트 형태로 만들 필요 없
  */
