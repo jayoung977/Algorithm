c71e226cc0e19b43bdfdb163f43a5d2844c81d34
jayoung choi <61008837+jayoung977@users.noreply.github.com>
2023-10-24 23:14:22 +0900

    [Gold III] Title: 서울 지하철 2호선, Time: 788 ms, Memory: 230604 KB -BaekjoonHub

--- 백준/Gold/16947. 서울 지하철 2호선/서울 지하철 2호선.java
+++ 백준/Gold/16947. 서울 지하철 2호선/서울 지하철 2호선.java
@@ -1,86 +1,80 @@
 import java.util.*;
- 
+import java.io.*;
 public class Main {
- 
-    static ArrayList<Integer>[] list;
-    static int n;
-    static boolean[] isCycle;
- 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
- 
-        n = scan.nextInt();
- 
-        list = new ArrayList[n + 1];
-        for(int i = 1; i <= n; i++) {
-            list[i] = new ArrayList<>();
-        }
- 
-        for(int i = 0; i < n; i++) {
-            int n1 = scan.nextInt();
-            int n2 = scan.nextInt();
-            list[n1].add(n2);
-            list[n2].add(n1);
-        }
- 
-        //싸이클을 찾아 표시해준다.
-        isCycle = new boolean[n + 1];
-        for(int i = 1; i <= n; i++) {
-            if(checkCycle(i, i, i)) break;
-            isCycle = new boolean[n + 1];
-        }
- 
-        int[] result = new int[n + 1];
-        for(int i = 1; i <= n; i++) {
-            if(!isCycle[i]) result[i] = bfs(i);
-        }
- 
-        for(int i = 1; i <= n; i++) System.out.print(result[i] + " ");
-    }
- 
-    public static int bfs(int node) {
-        Queue<Node> q = new LinkedList<>();
-        boolean[] visited = new boolean[n + 1];
-        q.add(new Node(node, 0));
-        visited[node] = true;
- 
-        while(!q.isEmpty()) {
-            Node current = q.poll();
-            if(isCycle[current.v]) return current.count;
- 
-            for(int i = 0; i < list[current.v].size(); i++) {
-                int next = list[current.v].get(i);
-                if(!visited[next]) {
-                    visited[next] = true;
-                    q.add(new Node(next, current.count + 1));
-                }
-            }
-        }
-        return 0;
-    }
- 
-    public static boolean checkCycle(int prev, int node, int start) {
-        isCycle[node] = true;
- 
-        for(int i = 0; i < list[node].size(); i++) {
-            int next = list[node].get(i);
- 
-            if(!isCycle[next]) {
-                if(checkCycle(node, next, start)) return true;
-            } else if(next != prev && next == start) return true;
-        }
-        isCycle[node] = false;
-        
-        return false;
-    }
- 
-    public static class Node {
-        int v;
-        int count;
- 
-        public Node(int v, int count) {
-            this.v = v;
-            this.count = count;
-        }
-    }
+	 static int N;
+	 static List<Integer>[] adj;
+	 static boolean[] isCycle;
+	 static class Node{
+		 int v;
+		 int d;
+		 Node(int v, int d){
+			 this.v=v;
+			 this.d=d;
+		 }
+	 }
+	 static Queue<Node> q;
+	 static boolean[] visited;
+	 public static void main(String[] args) throws Exception{
+		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		 StringTokenizer st;
+		 N = Integer.parseInt(br.readLine());
+		 adj = new ArrayList[N+1];
+		 for(int i=0; i<N+1; i++) adj[i]=new ArrayList<>();
+		 for(int i=0; i<N; i++) {
+			 st = new StringTokenizer(br.readLine());
+			 int s = Integer.parseInt(st.nextToken()); 
+			 int e = Integer.parseInt(st.nextToken()); 
+			 adj[s].add(e);
+			 adj[e].add(s);
+		 }
+		 isCycle = new boolean[N+1];
+		 for(int i=1; i<N+1; i++) {
+			 if(checkCycle_dfs(i,i,i)) break;
+			 isCycle = new boolean[N+1];
+		 }
+//		 System.out.println(Arrays.toString(isCycle));
+		 
+		 StringBuilder sb= new StringBuilder();
+		 for(int i=1; i<N+1; i++) {	  
+			 sb.append(bfs(i)).append(" ");		
+		 }
+		 System.out.println(sb.toString());
+
+	} 
+	static boolean checkCycle_dfs(int prev, int cur, int start) {
+		isCycle[cur]=true;
+		for(int i=0; i<adj[cur].size(); i++) {
+			int next = adj[cur].get(i);
+			if(!isCycle[next]) { // F
+				if(checkCycle_dfs(cur,next,start)) return true;
+			}else if(prev!=next && next==start ) {// T 
+				return true;
+			}
+		}
+		isCycle[cur]=false;
+		
+		return false;
+	}
+	
+	static int bfs(int v) {
+		if(isCycle[v]) return 0;
+		q = new LinkedList<>();
+		visited = new boolean[N+1];
+		q.offer(new Node(v,0));
+		visited[v]=true;
+		while(!q.isEmpty()) {
+			Node cur = q.poll();
+			if(isCycle[cur.v]) return cur.d;
+			for(int i=0; i<adj[cur.v].size(); i++) {
+				int next = adj[cur.v].get(i);
+				if(!visited[next]) {
+					visited[next]=true;
+					q.offer(new Node(next,cur.d+1));
+				}
+			}
+		}
+		
+		return 0;
+	}
+	
 }
