cb585151ba9f1b55bfde197160d82a41a9d40e1d
jayoung choi <61008837+jayoung977@users.noreply.github.com>
2023-12-14 17:38:23 +0900

    [Gold V] Title: 최소비용 구하기, Time: 344 ms, Memory: 123324 KB -BaekjoonHub

---
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -0,0 +1,26 @@
+import heapq
+n = int(input())
+m = int(input())
+INF = int(1e9)
+graph = [[] for _ in range(n+1)]
+distance = [INF]*(n+1)
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
+
+start, end = map(int, input().split())
+q = []
+heapq.heappush(q, (0, start))
+distance[start] = 0
+
+while q:
+    dist, now = heapq.heappop(q)
+    if distance[now] < dist:
+        continue
+    for i in graph[now]:
+        if (i[1]+dist < distance[i[0]]):
+            heapq.heappush(q, (i[1]+dist, i[0]))
+            distance[i[0]] = i[1]+dist
+
+print(distance[end])
