82034c651fddd31ff3c3b24e5a50fca8b904ed3a
jayoung choi <61008837+jayoung977@users.noreply.github.com>
2024-01-05 15:29:37 +0900

    [Gold V] Title: 강의실 배정, Time: 496 ms, Memory: 143512 KB -BaekjoonHub

---
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -0,0 +1,19 @@
+import heapq
+n = int(input())
+q = []
+for i in range(n):
+    s, e = map(int, input().split())
+    q.append([s, e])
+
+q.sort()
+
+room = []
+heapq.heappush(room, q[0][1])
+
+for i in range(1, n):
+    if q[i][0] < room[0]:
+        heapq.heappush(room, q[i][1])
+    else:
+        heapq.heappop(room)
+        heapq.heappush(room, q[i][1])
+print(len(room))
