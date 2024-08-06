fdbc8a6de2ed64e4a1f865d905e470cb61ced33f
jayoung choi <61008837+jayoung977@users.noreply.github.com>
2024-08-06 13:14:12 +0900

    [level 2] Title: 전화번호 목록, Time: 87.06 ms, Memory: 30.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,20 +1,6 @@
 def solution(phone_book):
     phone_book.sort()
-
-    for p1,p2 in zip(phone_book,phone_book[1:]):
-        if p2.startswith(p1) :
+    for x,y in zip(phone_book,phone_book[1:]):
+        if y.startswith(x):
             return False
-    
-        
-        
-    # while(cnt<len(phone_book)-1):
-    #     part = phone_book[cnt]
-    #     h = hash(part)
-    #     for i in range(cnt+1, len(phone_book)):
-    #         if part[0] != phone_book[i][0]:
-    #             break
-    #         if h - hash(phone_book[i][:len(part)]) == 0:
-    #             return False
-    #     cnt+=1
-
     return True
