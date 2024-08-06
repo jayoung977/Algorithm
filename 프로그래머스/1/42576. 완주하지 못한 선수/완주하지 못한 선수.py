e301672cee9094d4dcb81c50c1175b99c919e7d4
jayoung choi <61008837+jayoung977@users.noreply.github.com>
2024-08-06 13:11:52 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 78.92 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,5 +1,3 @@
 from collections import Counter
 def solution(participant, completion):
-    return list((Counter(participant) - Counter(completion)).keys())[0]
-
-
+    return list(Counter(participant)-Counter(completion))[0]
