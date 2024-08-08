b02fcd91f4b4fa5ea865f2a68f6f1a2a61b5e632
jayoung choi <61008837+jayoung977@users.noreply.github.com>
2024-08-08 14:38:19 +0900

    Update 다리를 지나는 트럭.py

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -3,7 +3,8 @@ def solution(bridge_length, weight, truck_weights):
     trucks = deque(truck_weights)
     bridge = deque() 
     answer = 0
-    total_weight = 0
+    #초기값 넣는 부분이 안될 수도 있는게 길이가 1이면 다음값 뺼 때 에러남
+    total_weight = 0 #sum 대신 이방법이 시간초과 안남
 
     while trucks:
         next_truck = trucks[0]
@@ -14,14 +15,10 @@ def solution(bridge_length, weight, truck_weights):
         else:
             bridge.append(0)
         answer+=1
-        # print(bridge,trucks,answer)
         
         if len(bridge) == bridge_length:
             total_weight -= bridge.popleft()
             
-    answer+=bridge_length
-            
-        
-    
+    answer+=bridge_length #이부분 놓쳤었음
     
     return answer
