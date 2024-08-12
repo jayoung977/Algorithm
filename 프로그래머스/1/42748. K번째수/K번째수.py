eb19d2dc64a505c4bb5e730a627990bb0f6034b5
jayoung choi <61008837+jayoung977@users.noreply.github.com>
2024-08-12 11:08:59 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,8 +1,6 @@
 def solution(array, commands):
     answer = []
     for i,j,k in commands:
-        part = array[i-1:j]
-        part.sort()
-        answer.append(part[k-1])
-    
+        new_array = sorted(array[i-1:j])
+        answer.append(new_array[k-1])
     return answer
