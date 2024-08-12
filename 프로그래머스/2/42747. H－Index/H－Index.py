c4b40e1c77f38700764d14c376abef3544a8f462
jayoung choi <61008837+jayoung977@users.noreply.github.com>
2024-08-12 11:24:23 +0900

    [level 2] Title: H-Index, Time: 7.10 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,9 +1,8 @@
 def solution(citations):
-    paper_len = len(citations)
-    answer = 0
-    for i in range(1, paper_len+1):
-        papers = list(filter(lambda x: x>=i,citations))
-        if len(papers) >= i:
-            answer = i
-    
-    return answer
+    len_cite = len(citations)
+    for i in range(len_cite+1,1,-1):
+        if len(list(filter(lambda x: x>=i,citations))) >= i:
+            return i
+    return 0
+        
+        
--- 프로그래머스/2/42747. H－Index/README.md
+++ 프로그래머스/2/42747. H－Index/README.md
@@ -1,10 +1,10 @@
 # [level 2] H-Index - 42747 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 88.22 ms
+메모리: 10.2 MB, 시간: 7.10 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 09일 15:10:29
+2024년 08월 12일 11:24:21
 
 ### 문제 설명
 
