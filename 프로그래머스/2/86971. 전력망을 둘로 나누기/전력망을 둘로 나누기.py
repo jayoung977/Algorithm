01335a4c1a0841a5992bfab6cbe0ea36f52159df
jayoung choi <61008837+jayoung977@users.noreply.github.com>
2024-08-13 14:32:48 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 3.88 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,35 +1,29 @@
-def dfs(n,graph,s,visited):
-    visited[s] = True
-    for e in graph[s]:
-        if visited[e]==False:
-            dfs(n,graph,e,visited)
-
+def dfs(num):
+    global visited,result,li
+    for i in li[num]:
+        if not visited[i]:
+            visited[i]=True
+            result+=1           
+            dfs(i)
+    return 
 
 def solution(n, wires):
-    graph_list = []
-    s_list = []
-    for i in range(len(wires)):
-        new_wires = wires.copy()
-        new_wires.remove(wires[i])
-        # print("new_wires:",new_wires)
-        graph = [[] for i in range(n+1)]
-        flag = False
-        for node in new_wires:
-            s,e = node
-            graph[s].append(e)
-            graph[e].append(s)
-            if flag==False:
-                s_list.append(s)
-                flag = True
-        graph_list.append(graph)
-    # print("s_list:",s_list)
-    # print("graph_list",len(graph_list))
+    global visited,result,li
+    li = [[] for _ in range(n+1)]
+    
     answer = 1e9
-    for i in range(len(graph_list)):
-        graph = graph_list[i]
-        s = s_list[i]
-        visited = [False]*(n+1) 
-        dfs(n,graph,s,visited)
-        result = abs(visited[1:].count(False)-visited[1:].count(True))
-        answer = min(result,answer)        
+    for s,e in wires:
+        li[s].append(e)
+        li[e].append(s)
+        
+    for s,e in wires:
+        li[s].remove(e)
+        li[e].remove(s)
+        visited = [False]*(n+1)
+        visited[1]=True
+        result = 1
+        dfs(1)
+        answer = min(abs(2*result-n),answer)
+        li[s].append(e)
+        li[e].append(s)
     return answer
