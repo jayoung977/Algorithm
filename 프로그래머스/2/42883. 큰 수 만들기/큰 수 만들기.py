58de36af53e8b61a35a0bc83a68f8b312cfe6d25
jayoung choi <61008837+jayoung977@users.noreply.github.com>
2024-08-14 15:17:38 +0900

    [level 2] Title: 큰 수 만들기, Time: 115.34 ms, Memory: 12.9 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -1,14 +1,10 @@
 def solution(number, k):
     stack = []
-    for i, num in enumerate(number):
-        # 스택의 마지막 숫자가 현재 숫자보다 작으면 제거
-        while stack and k > 0 and stack[-1] < num:
+    for n in number:
+        while stack and  k>0 and n>stack[-1]:
             stack.pop()
-            k -= 1
-        stack.append(num)
-    
-    # 만약 제거할 숫자가 남아있다면 뒤에서부터 제거
-    if k > 0:
-        stack = stack[:-k]
-    
+            k-=1
+        stack.append(n)
+    if k>0:
+        stack=stack[:-k]
     return ''.join(stack)
