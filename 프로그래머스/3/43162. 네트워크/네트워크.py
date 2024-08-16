20653219fb27776d14b9afc8f8154ecc64ca9961
jayoung choi <61008837+jayoung977@users.noreply.github.com>
2024-08-16 13:09:59 +0900

    [level 3] Title: 네트워크, Time: 0.53 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,42 +1,18 @@
-def dfs(n,computers,s,visited):
-    visited[s]=True
-    for e in range(n):
-        if computers[s][e]==1 and visited[e]==False:
-            dfs(n,computers,e,visited)
-    
-    
-            
+visited = []
+def dfs(i,computers):
+    visited[i]=True
+    for j,is_connect in enumerate(computers[i]):
+        if j!=i and is_connect==1 and not visited[j]:
+            visited[j]=True
+            dfs(j,computers)
+    return
+
 def solution(n, computers):
+    global visited
     visited = [False]*n
     answer = 0
-    for s in range(n):      
-        if visited[s] == False:
-            print(s)
-            print(visited)
-            dfs(n,computers,s,visited)
-            answer+=1
-                
+    for i in range(n):
+        if not visited[i]:
+            dfs(i,computers)
+            answer+=1    
     return answer
-
-
-
-
-# answer = 0
-# def dfs(n,computers,r,c,d,visited):
-    
-#     for i in range(c+1,n):
-#         if computers[c][i]==1:
-#             visited[i]=visited[c]
-#             dfs(n,computers,c,i,d+1,visited)
-
-# def solution(n, computers):
-#     visited = 
-#     for i in range(n):
-#         for j in range(i+1, n):
-#             if computers[i][j]==1 and visited[j]==j:
-#                 visited[i],visited[j] = i,i
-#                 con = dfs(n,computers,i,j,0,visited)
-#                 print(visited)
-#         answer = min(answer,len(set(visited)))
-                
-#     return answer
