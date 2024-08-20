b4dfacf464956e8cd466cf7b8cf93230cd0ba461
jayoung choi <61008837+jayoung977@users.noreply.github.com>
2024-08-20 23:08:49 +0900

    [level 3] Title: 입국심사, Time: 710.83 ms, Memory: 14.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -0,0 +1,19 @@
+def solution(n, times):
+    times.sort()
+    left = 0
+    right = max(times)*n
+    answer = 0
+    while left<=right:
+        mid = (left+right)//2
+        people = 0
+        for time in times:
+            people+=mid//time
+            if people >=n:
+                break
+        if people >=n:
+            right=mid-1
+            answer = mid
+        else:
+            left=mid+1
+
+    return answer
