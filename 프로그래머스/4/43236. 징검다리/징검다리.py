f66b8cd836878292564ebaa2396cd09fa0073168
jayoung choi <61008837+jayoung977@users.noreply.github.com>
2024-08-21 09:43:16 +0900

    [level 4] Title: 징검다리, Time: 123.70 ms, Memory: 11.6 MB -BaekjoonHub

---
+++ 프로그래머스/4/43236. 징검다리/징검다리.py
@@ -0,0 +1,24 @@
+def solution(distance, rocks, n):
+    left = 1
+    right = distance
+    rocks.append(distance)
+    rocks.sort()
+    answer=0
+    while left<=right:
+        mid = (left+right)//2
+        prev_rock = 0
+        delete = 0
+        for rock in rocks:
+            dist = rock-prev_rock
+            if dist<mid:
+                delete+=1
+                if delete>n:
+                    break
+            else:
+                prev_rock=rock
+        if delete>n:
+            right = mid-1
+        else:
+            answer = mid
+            left = mid+1
+    return answer
