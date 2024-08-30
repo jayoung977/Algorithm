5d0619ea5c42839a213a9a1b75abc89d87a280c0
jayoung choi <61008837+jayoung977@users.noreply.github.com>
2024-08-30 16:08:19 +0900

    [Gold V] Title: 감시 피하기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/18428. 감시 피하기/감시 피하기.py
+++ 백준/Gold/18428. 감시 피하기/감시 피하기.py
@@ -1,81 +1,54 @@
-import copy
+from itertools import combinations
+
+def isFind(r,c):
+    d = 0
+    while d < 4:
+        nr,nc = r+dr[d],c+dc[d]
+        while True:
+            if not(0<=nr<N and 0<=nc<N):
+                break
+            if data[nr][nc]=="O":
+                break
+            elif data[nr][nc]=="S":
+                return True
+            nr+=dr[d]
+            nc+=dc[d]
+        d+=1
+    return False
+
+def solution(N,data):
+    T_list, X_list = [],[]
+    for i in range(N):
+        for j in range(N):
+            if data[i][j]=="S":
+                continue
+            elif data[i][j]=="T":
+                T_list.append((i,j))
+                continue
+            X_list.append((i,j))
+    for combi in combinations(X_list,3):
+        #넣기
+        for r,c in combi:
+            data[r][c]="O"
+        flag = True
+        for r,c in T_list:
+            if isFind(r,c):
+                flag=False
+                break
+        if flag:
+            return "YES"
+        #뺴기
+        for r,c in combi:
+            data[r][c]="X"
+            
+
+    return "NO"
 
 
-def combi(start, r, chosen):
-    global combi_list
-    if (len(chosen) == r):
-        combi_list.append(chosen[:])
-        return
-    for i in range(start, len(X_arr)):
-        chosen.append(X_arr[i])
-        combi(i+1, r, chosen)
-        chosen.pop()
-
-
-dr = [-1, 0, 1, 0]
-dc = [0, 1, 0, -1]
-
-
-def check(r, c):
-    idx = 0
-    nr = r
-    nc = c
-    while (idx < 4):
-        nr += dr[idx]
-        nc += dc[idx]
-        # print(nr, nc)
-        if (not (0 <= nr < N and 0 <= nc < N)):
-            idx += 1
-            nr = r
-            nc = c
-            continue
-        if (temp[nr][nc] == 'O'):
-            idx += 1
-            nr = r
-            nc = c
-            continue
-        if (temp[nr][nc] == 'S'):
-            return False
-    return True
 
 
 N = int(input())
 data = [list(input().split()) for _ in range(N)]
-X_arr = [(r, c) for r in range(N) for c in range(N) if data[r][c] == 'X']
-T_arr = [(r, c) for r in range(N) for c in range(N) if data[r][c] == 'T']
-
-answer = "NO"
-
-# 조합
-combi_list = []
-# 다시
-combi(0, 3, [])
-
-# test
-# combi_list = [[(0, 3), (1, 1), (2, 2)]]
-# check
-for chosen in combi_list:
-    temp = copy.deepcopy(data)
-    # O표시
-    for r, c in chosen:
-        temp[r][c] = 'O'
-    # for i in range(N):
-        # print(temp[i])
-    isNo = False
-    # 선생님이면 DFS
-    for r, c in T_arr:
-        if (not check(r, c)):
-            isNo = True
-            break
-    if (isNo):
-        continue
-    else:
-        answer = "YES"
-        break
-print(answer)
-# 5
-# X S X X T
-# T X S X X
-# X X X X X
-# X T X X X
-# X X T X X
+dr = [-1,0,1,0]
+dc = [0,1,0,-1]
+print(solution(N,data))
