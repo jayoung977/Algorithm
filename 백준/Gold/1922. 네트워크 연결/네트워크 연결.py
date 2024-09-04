bf3a5ebfa181a31e5467ccc011ecc79ca34ecea3
jayoung choi <61008837+jayoung977@users.noreply.github.com>
2024-09-05 00:00:50 +0900

    [Gold IV] Title: 네트워크 연결, Time: 2712 ms, Memory: 69076 KB -BaekjoonHub

--- 백준/Gold/1922. 네트워크 연결/네트워크 연결.py
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.py
@@ -1,30 +1,27 @@
+from heapq import heappush,heappop
 N = int(input())
 M = int(input())
-Edges = []
+G = [[] for _ in range(N+1)]
 for _ in range(M):
     a,b,c = map(int,input().split())
-    Edges.append((c,a,b))
-Edges.sort()
-parent = [i for i in range(N+1)]
+    G[a].append((b,c))
+    G[b].append((a,c))
 
-def find(parent,x):
-    if parent[x]!=x:
-        parent[x]=find(parent,parent[x])
-    return parent[x]
+def prim(start):
+    q = []
+    visited = [0]*(N+1)
+    heappush(q,(0,start))
+    cnt = 0
+    ans = 0
+    while cnt < N:
+        w,v = heappop(q)
+        if visited[v]:
+            continue
+        visited[v]=1
+        ans+=w
+        cnt+=1
+        for nv,nw in G[v]:
+            heappush(q,(nw,nv))
 
-def union(parent,a,b):
-    a = find(parent,a)
-    b = find(parent,b)
-    if a<b:
-        parent[b]=a
-        return
-    parent[a]=b
-    return
-
-ans = 0
-for c,a,b in Edges:
-    if find(parent,a)!=find(parent,b):
-        union(parent,a,b)
-        ans+=c
-        
-print(ans)
+    return ans
+print(prim(1))
