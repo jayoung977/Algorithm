3cd2849d836c860b850ab90901e2c50c25ce07b7
jayoung choi <61008837+jayoung977@users.noreply.github.com>
2024-09-05 15:03:49 +0900

    [level 2] Title: 프로세스, Time: 0.16 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,13 +1,25 @@
 from collections import deque
+from heapq import heappush,heappop
 def solution(priorities, location):
-    queue = deque((i,p) for i,p in enumerate(priorities))
-    answer = 0
-    while queue:
-        cur = queue.popleft()
-        if any(cur[1] < p for i,p in queue):
-            queue.append(cur)
+    q = deque()
+    hq = []
+    for i,p in enumerate(priorities):
+        q.append((p,i))
+        heappush(hq,(-p,i))
+    answer = []
+    while q:
+        if q[0][0]!=-hq[0][0]:
+            while q[0][0]!=-hq[0][0]:
+                cur = q.popleft()
+                q.append(cur)
         else:
-            answer+=1
-            if cur[0]==location:
-                return answer
-    return answer
+            # print(q[0])
+            cur = q.popleft()
+            heappop(hq)
+            answer.append(cur[1])
+            # print(answer)
+            
+        
+        
+
+    return answer.index(location)+1
