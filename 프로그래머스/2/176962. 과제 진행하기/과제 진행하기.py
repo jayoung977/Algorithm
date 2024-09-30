e5f64860b2a3630a1eee1c2e449173151fabbc15
jayoung choi <61008837+jayoung977@users.noreply.github.com>
2024-09-30 15:09:02 +0900

    [level 2] Title: 과제 진행하기, Time: 3.30 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.py
+++ 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.py
@@ -1,23 +1,30 @@
 def solution(plans):
-    stack = []
+    def convert_time(str):
+        h,m = map(int,str.split(":"))
+        return h*60+m
+    
     answer = []
     for i in range(len(plans)):
-        h, m = map(int, plans[i][1].split(':'))
-        plans[i][1] = h*60 + m
-        plans[i][2] = int(plans[i][2])
-    plans.sort(key=lambda x: x[1])
+        plans[i][1]=convert_time(plans[i][1])
+        plans[i][2]=int(plans[i][2])
+    plans.sort(key=lambda x:x[1])
+    
+    stack = []
     for i in range(len(plans)-1):
-        stack.append([plans[i][0], plans[i][2]])
-        gap = plans[i+1][1] - plans[i][1]
+        gap = plans[i+1][1]-plans[i][1]
+        stack.append([plans[i][0],plans[i][2]])
         while stack and gap:
-            if stack[-1][1] <= gap:
-                cn, ct = stack.pop()
-                gap -= ct
-                answer.append(cn)
+            if stack[-1][1]<=gap:
+                gap-=stack[-1][1]
+                answer.append(stack[-1][0])
+                stack.pop()
             else:
-                stack[-1][1] -= gap
-                gap = 0
+                stack[-1][1]-=gap
+                gap=0
     answer.append(plans[-1][0])
     for i in range(len(stack)):
         answer.append(stack[~i][0])
+    
+                
+        
     return answer
