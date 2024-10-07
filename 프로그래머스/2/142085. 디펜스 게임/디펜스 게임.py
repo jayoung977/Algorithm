ecc225f72a103dfdb1212ab6b158dda92110cb20
jayoung choi <61008837+jayoung977@users.noreply.github.com>
2024-10-07 18:31:43 +0900

    [level 2] Title: 디펜스 게임, Time: 403.34 ms, Memory: 53.1 MB -BaekjoonHub

--- 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.py
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.py
@@ -7,22 +7,12 @@ def solution(n, k, enemy):
 
     for i in range(len(enemy)):
         defence += enemy[i]
-        heappush(hq, -enemy[i])  # 인덱스는 필요 없으므로 enemy[i]만 삽입
-        # print("i:{},delete:{},defence:{},hq:{}".format(i,delete,defence,hq))
-        # defence가 n 이상일 경우 방어할 수 없으므로 병력 소모를 통해 조정
+        heappush(hq, -enemy[i])  
         while defence > n and delete < k:
-            defence -= -heappop(hq)  # 가장 작은 적의 병력을 제거
+            defence -= -heappop(hq) 
             delete += 1
-            # print("--delete:{},defence:{},hq:{}".format(delete,defence,hq))
-            
-        
-
-        # 방어 불가능하면 종료
         if defence > n and delete==k:
             break
         else:
-            answer = i + 1  # 성공한 라운드 수를 기록
-            # print("answer:{}".format(answer))
-            
-
+            answer = i + 1 
     return answer
