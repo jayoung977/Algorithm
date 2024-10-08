cbff0677ba5461808e23ab3483f1e7262dbf217e
jayoung choi <61008837+jayoung977@users.noreply.github.com>
2024-10-08 17:51:13 +0900

    [level 2] Title: 숫자 변환하기, Time: 565.51 ms, Memory: 92 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
@@ -1,27 +1,14 @@
 from collections import deque
-def solution(x, y, n):   
-    answer = 1e9
-    q = deque()
-    q.append((x,0))
-    visited = set()
+def solution(x, y, n):
+    visited=set()
+    q = deque([(x,0)])
     visited.add(x)
     while q:
-        num,cnt=q.popleft()
-        if num==y:
-            answer=cnt
-            break
-        if num+n<=y and num+n not in visited:
-            q.append([num+n,cnt+1])
-            visited.add(num+n)
-        if num*2<=y and num*2 not in visited:
-            q.append([num*2,cnt+1])
-            visited.add(num*2)
-        if num*3<=y and num*3 not in visited:
-            q.append([num*3,cnt+1])
-            visited.add(num*3)
-    
-    if answer == 1e9:
-        answer=-1
-        
-    
-    return answer
+        x,cnt=q.popleft()
+        if x==y:
+            return cnt
+        for nx in (x+n,x*2,x*3):
+            if nx not in visited and nx<=y:
+                visited.add(nx)
+                q.append((nx,cnt+1))
+    return -1
