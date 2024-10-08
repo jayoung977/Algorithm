21fc4708aa19667c08f8ec8d7084061ed0ff5d48
jayoung choi <61008837+jayoung977@users.noreply.github.com>
2024-10-08 13:37:58 +0900

    [level 3] Title: 단어 변환, Time: 0.32 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,19 +1,25 @@
-from collections import Counter,deque
+from collections import deque
 def solution(begin, target, words):
+    q = deque([(begin,0)])
+    visited = set()
+    visited.add(begin)
     answer = 0
-    queue = deque()
-    queue.append((begin,0))
-    visited = []
-    visited.append(begin)
-    if target not in words:
-        return 0
-    while queue:
-        cur,cnt = queue.popleft()
-        for w in words:
-            if len(Counter(cur)-Counter(w))==1 and w not in visited:
-                if w == target:
-                    return cnt+1
-                visited.append(w)
-                queue.append((w,cnt+1))
-                
+    while q:
+        w,cnt=q.popleft()
+        if w == target:
+            answer = cnt
+            break
+        for nw in words:
+            if nw in visited:
+                continue
+            not_eq = 0
+            for idx, nww in enumerate(nw):
+                if w[idx]!=nww:
+                    not_eq+=1
+                if not_eq>1:
+                    break
+            if not_eq==1:
+                visited.add(nw)
+                q.append((nw,cnt+1))
+            
     return answer
