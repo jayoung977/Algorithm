2596b54802a7f92ff3b26bc44f53c0a43f9eec29
jayoung choi <61008837+jayoung977@users.noreply.github.com>
2024-10-08 13:19:55 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 11.72 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,26 +1,23 @@
 from collections import deque
-dr = [-1,0,1,0]
-dc = [0,1,0,-1]
 def solution(maps):
-    answer = 0
-    m,n = len(maps),len(maps[0])
-    queue = deque()
-    visited = [[False]*n for _ in range(m)]
-    queue.append((0,0,1))
-    visited[0][0]=True
-    while queue:
-        r,c,cnt = queue.popleft()
-        # print(r,c,cnt)
-        answer = max(cnt,answer)
+    
+    n,m = len(maps),len(maps[0])
+    dr,dc = [0,1,0,-1], [-1,0,1,0]
+    q = deque([(0,0,1)])
+    visited = [[0]*m for _ in range(n)]
+    visited[0][0]=1
+    answer = -1
+    while q:
+        r,c,cnt=q.popleft()
+        if r==n-1 and c==m-1:
+            answer=cnt
+            break
         for d in range(4):
-            nr = r+dr[d]
-            nc = c+dc[d]
-            if not(0<=nr<m and 0<=nc<n):
+            nr,nc=r+dr[d],c+dc[d]
+            if not(0<=nr<n and 0<=nc<m):
                 continue
-            if not visited[nr][nc] and maps[nr][nc]==1:
-                if nr==m-1 and nc==n-1:
-                    return cnt+1
-                visited[nr][nc]=True
-                queue.append((nr,nc,cnt+1))
-            
-    return -1
+            if visited[nr][nc] or not maps[nr][nc]:
+                continue
+            visited[nr][nc]=1
+            q.append((nr,nc,cnt+1))
+    return answer
