4172f576b1a6360048530783f6422bf02bb5b4fa
jayoung choi <61008837+jayoung977@users.noreply.github.com>
2024-10-10 11:57:52 +0900

    [level 1] Title: 체육복, Time: 0.09 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,19 +1,22 @@
-def solution(n, lost, reserve): 
+from collections import Counter
+def solution(n, lost, reserve):
+    answer = n-len(lost)
     lost.sort()
     reserve.sort()
-    answer = n-len(lost)
-    for l in lost:
-        if l in reserve:
+    # 여벌 체육복을 가져온 학생이 체육복을 도난당한 경우, 배열에서 제외
+    count = Counter(reserve)+Counter(lost)
+    for k,v in count.items():
+        if v == 2:
             answer+=1
-            reserve.remove(l)
-        elif l-1 in reserve:
-            answer+=1
-            reserve.remove(l-1)
-            # continue
-        elif l+1 in reserve and l+1 not in lost:
-            answer+=1
-            reserve.remove(l+1)
-            # continue
-        # print(reserve)
+            lost.remove(k)
+            reserve.remove(k)
             
+    for r in reserve:
+        if r-1 in lost:
+            answer+=1
+            lost.remove(r-1)
+        elif r+1 in lost:
+            answer+=1
+            lost.remove(r+1)
+                
     return answer
