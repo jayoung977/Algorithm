d1fae9697fba22a51bdde5eeeb81be996a84648c
jayoung choi <61008837+jayoung977@users.noreply.github.com>
2024-10-16 20:26:47 +0900

    [level 2] Title: 조이스틱, Time: 0.05 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.py
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.py
@@ -1,13 +1,22 @@
-def cal(word):
-    return min(ord(word)-ord("A"), ord("Z")-ord(word)+1)
-
+def min_updouwn(alpha):
+    up = ord(alpha)-ord("A")
+    down = ord("Z")-ord(alpha)+1
+    return min(up,down)
+    
 def solution(name):
-    answer = 0
-    min_cursor = len(name)-1
-    for i,word in enumerate(name):
-        answer+=cal(word)
-        next = i+1
-        while next<len(name) and name[next]=="A":
-                next+=1
-        min_cursor = min([min_cursor,2*i+(len(name)-next),2*(len(name)-next)+i])
-    return answer + min_cursor
+    
+    sum_updown=0
+    for alpha in name:
+        sum_updown+=min_updouwn(alpha)
+    
+    min_leftright = len(name)-1
+    for i in range(len(name)):
+        cursor=i+1
+        while cursor<len(name) and name[cursor]=="A":
+            cursor+=1
+
+        right2left = i * 2 + (len(name) - cursor)
+        left2right = i + (len(name) - cursor) * 2
+        min_leftright = min([right2left,left2right,min_leftright])
+
+    return sum_updown+min_leftright
