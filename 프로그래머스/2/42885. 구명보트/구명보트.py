09d8b5e875061cafb5f1876941b5947556a3c4f0
jayoung choi <61008837+jayoung977@users.noreply.github.com>
2024-10-17 13:33:34 +0900

    [level 2] Title: 구명보트, Time: 9.20 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,20 +1,16 @@
-import heapq
-from collections import deque
 def solution(people, limit):
-    answer = 0
-    # heapq.heapify(people)
     people.sort()
-    queue = deque(people)
-    # left_cursor = 0
-    # right_cursor = len(people)-1
-    # while people:
-    #     heapq.heappop(people)
-    while len(queue)>=2:
-        right = queue.pop()
-        if queue[0]+right<=limit:
-            queue.popleft()
+    answer = 0
+    left,right = 0,len(people)-1
+    while left<=right:
+        if people[left]+people[right]<=limit:
+            left+=1
+        right-=1
         answer+=1
-    if queue:
-        answer+=1
+            
+            
+        
+            
 
+            
     return answer
