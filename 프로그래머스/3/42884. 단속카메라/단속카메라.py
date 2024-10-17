07260445ae1358584889d1ff209d46bf0098524c
jayoung choi <61008837+jayoung977@users.noreply.github.com>
2024-10-17 14:44:59 +0900

    [level 3] Title: 단속카메라, Time: 0.97 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.py
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.py
@@ -1,13 +1,8 @@
-from collections import deque
 def solution(routes):
-    
-    routes.sort(key=lambda x:(x[1],x[0]))
-    queue = deque(routes)
-    answer = 0
-    while queue:
-        current = queue.popleft()
-        answer+=1
-        while queue and queue[0][0]<=current[1]<=queue[0][1]:
-            
-            queue.popleft()
-    return answer
+    routes.sort(key=lambda x: x[1])
+    stack = []
+    for s,e in routes:
+        if stack and stack[-1]>=s:
+            continue
+        stack.append(e)
+    return len(stack)
