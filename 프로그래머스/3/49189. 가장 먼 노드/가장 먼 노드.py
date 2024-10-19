ef051cc320eb3e5efce4378c0c20b5e7f0565364
jayoung choi <61008837+jayoung977@users.noreply.github.com>
2024-10-19 23:08:14 +0900

    [level 3] Title: 가장 먼 노드, Time: 48.47 ms, Memory: 21 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -1,19 +1,35 @@
-from collections import deque,Counter
+from heapq import heappush,heappop
 def solution(n, edge):
-    answer = 0
-    nodes = [[] for _ in range(n+1)]
-    for a,b in edge:
-        nodes[a].append(b)
-        nodes[b].append(a)
+    graph = [[] for _ in range(n+1)] #1번부터
+    for s,e in edge:
+        graph[s].append(e)
+        graph[e].append(s)
     
-    visited = [0]*(n+1)
-    queue = deque()
-    queue.append((1,0))
-    visited[1]=1
-    while queue:
-        node, cnt = queue.popleft()
-        for nex in nodes[node]:
-            if visited[nex]==0:
-                queue.append((nex,cnt+1))
-                visited[nex]=cnt+1    
-    return Counter(visited)[max(visited)]
+    distance = [int(1e9)]*(n+1) #0번 있음 조심
+    distance[1]=0
+    hq = []
+    heappush(hq,(0,1))
+    while hq:
+        dist,node = heappop(hq)
+        if distance[node]<dist:
+            continue
+        for nnode in graph[node]:
+            ndist = dist+1
+            if ndist<distance[nnode]:
+                distance[nnode]=ndist
+                heappush(hq,(ndist,nnode))
+    max_d = max(distance[1:])
+    answer = 0
+    for i in range(1,len(distance)):
+        if distance[i]==max_d:
+            answer+=1
+        
+                
+        
+    
+    
+    
+    
+        
+    
+    return answer
