e0a0e19d6ce6bfb6add04ac349ce6a15aead3619
jayoung choi <61008837+jayoung977@users.noreply.github.com>
2024-10-19 20:05:53 +0900

    [level 2] Title: 기능개발, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -2,16 +2,11 @@ from math import ceil
 def solution(progresses, speeds):
     answer = []
     stack = []
-    idx = 0
-    for i,(p,s) in enumerate(zip(progresses,speeds)):
-        day = ceil((100-p)/s)
-        # print("stack:{},idx:{},day:{}".format(stack,idx,day))
-        if stack and stack[-1]>=day:
+    for p,s in zip(progresses,speeds):
+        days = ceil((100-p)/s)
+        if stack and stack[-1]>=days:
             answer[-1]+=1
             continue
-        stack.append(day)
         answer.append(1)
-        # print("answer:{}".format(answer))
-            
-        
+        stack.append(days)
     return answer
