9c6e42a6355160649737480bf70e5c396a528f0e
jayoung choi <61008837+jayoung977@users.noreply.github.com>
2024-10-29 15:12:26 +0900

    [level 2] Title: 구명보트, Time: 10.61 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,16 +1,15 @@
 def solution(people, limit):
     people.sort()
     answer = 0
-    left,right = 0,len(people)-1
+    left,right =0,len(people)-1
     while left<=right:
         if people[left]+people[right]<=limit:
+            answer+=1
             left+=1
-        right-=1
-        answer+=1
+            right-=1
+        else:
+            answer+=1
+            right-=1
             
-            
-        
-            
-
             
     return answer
