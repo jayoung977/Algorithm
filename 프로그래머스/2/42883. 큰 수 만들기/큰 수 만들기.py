e92ba34b74d6b47f1e31feeba1516b683b1e476f
jayoung choi <61008837+jayoung977@users.noreply.github.com>
2024-10-29 15:08:49 +0900

    [level 2] Title: 큰 수 만들기, Time: 110.95 ms, Memory: 13.1 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -1,12 +1,13 @@
 def solution(number, k):
-    stack=[]
-    for num in number:
-        while stack and k>0 and stack[-1]<num:
+    
+    stack = []
+    for n in number:
+        while k>0 and stack and stack[-1]<n:
             stack.pop()
             k-=1
-        stack.append(num)
+        stack.append(n)
     if k>0:
-        stack=stack[:-k]
+        stack = stack[:-k]
         
-    answer = ''.join(stack)
-    return answer
+        
+    return "".join(stack)
