0cd2bae9430d0d6ea6ec2ff8d7e4aaa303065d01
jayoung choi <61008837+jayoung977@users.noreply.github.com>
2024-10-29 16:41:33 +0900

    [level 2] Title: 소수 찾기, Time: 14.28 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,27 +1,26 @@
 from itertools import permutations
-def solution(numbers):
-    def isPrime(number):
-        if number in (0,1):
+def isPrime(num):
+    if num<2:
+        return False
+    for i in range(2,int(num**(1/2))+1):
+        if num%i==0:
             return False
-        
-        for i in range(2,int(number**(1/2))+1):
-            if i == 1:
-                continue
-            if number%i==0:
-                return False
-        return True
-    
+    return True
+
+def solution(numbers):
+    answer = 0
     li = list(numbers)
     visited = set()
-    answer = 0
     for n in range(1,len(li)+1):
         for perm in permutations(li,n):
-            number = int("".join(perm))
-            if number in visited:
+            num = ""
+            for p in perm:
+                num+=p
+            num = int(num)   
+            if num in visited:
                 continue
-            visited.add(number)
-            if isPrime(number):
-                answer+=1
+            visited.add(num)
             
-    
+            if isPrime(num):
+                answer+=1
     return answer
