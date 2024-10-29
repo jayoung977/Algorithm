fe830e226b6c23015e97b13e48445caca926b080
jayoung choi <61008837+jayoung977@users.noreply.github.com>
2024-10-29 16:22:37 +0900

    [level 3] Title: 단속카메라, Time: 2.29 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.py
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.py
@@ -1,8 +1,10 @@
 def solution(routes):
-    routes.sort(key=lambda x: x[1])
-    stack = []
+    routes.sort(key=lambda x:(x[1],x[0]))
+    stack=[]
+    answer = 0
     for s,e in routes:
         if stack and stack[-1]>=s:
             continue
         stack.append(e)
+            
     return len(stack)
